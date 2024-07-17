ffa0587852ce75b4fc3112224ff9cd6bdd7b1bea
max judkevitch <maxjud2904@gmail.com>
2024-07-17 15:12:35 +0300

    version 1.1 finished outdoor frontend

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
 import ContactMe from "./pages/ContactMe/ContactMe";
@@ -15,7 +15,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <HashRouter>
       <NavigationBar />
       <Routes>
         <Route path="/" exact Component={Home} />
@@ -25,7 +25,7 @@ function App() {
         <Route path="/gallery" exact Component={Gallery} />
       </Routes>
       <Footer />
-    </Router>
+    </HashRouter>
   );
 }
 
