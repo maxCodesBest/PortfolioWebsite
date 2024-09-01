a274423b0a9563a1886d17e68caac124906f1cc0
max judkevitch <maxjud2904@gmail.com>
2024-09-01 18:10:35 +0300

    remove anchor in resume

--- portfolio-website/src/pages/Resume/Resume.js
+++ portfolio-website/src/pages/Resume/Resume.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Anchor } from "antd";
 import "./Resume.css";
 import Profile from "./sections/Profile.js";
 import EmploymentHistory from "./sections/EmploymentHistory.js";
@@ -9,55 +8,15 @@ import ScrollToTop from "../../components/ScrollToTop/ScrollToTop";
 import "../../commonFiles/commonText.css";
 
 export default function Resume() {
-  const offSet = 80;
   return (
     <>
       <ScrollToTop />
       <div className="resume">
-        <div className="resumeSide">
-          <div id="profile">
-            <Profile />
-          </div>
-          <div id="employmentHistory">
-            <EmploymentHistory />
-          </div>
-
-          <div className="doubleSection" id="languagesAndHobbies">
-            <Languages />
-            <Hobbies />
-          </div>
-        </div>
-
-        <div className="anchorSide">
-          <Anchor
-            offsetTop={offSet}
-            replace
-            items={[
-              {
-                key: "profile",
-                href: "#profile",
-                title: "Profile",
-              },
-              {
-                key: "employmentHistory",
-                href: "#employmentHistory",
-                title: "Employment History",
-                children: [
-                  {
-                    key: "TransmitEra",
-                    href: "#TransmitEra",
-                    title: "Transmit Security",
-                  },
-                  { key: "IdfEra", href: "#IdfEra", title: "IDF" },
-                ],
-              },
-              {
-                key: "languagesAndHobbies",
-                href: "#languagesAndHobbies",
-                title: "Languages And Hobbies",
-              },
-            ]}
-          />
+        <Profile />
+        <EmploymentHistory />
+        <div className="doubleSection">
+          <Languages />
+          <Hobbies />
         </div>
       </div>
     </>
